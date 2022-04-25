80da3247c1cda2d5ef88a15bf7c329b3ad767fea
ijiefeng <yijie4188@gmail.com>
2022-04-25 15:32:02 +0800

    fix(core): unable infer Query

--- packages/core/src/query.ts
+++ packages/core/src/query.ts
@@ -3,16 +3,14 @@ import { Model } from './model'
 export type Query<S extends Model.Schema | unknown> = {
   $or?: Query<S>[]
   $and?: Query<S>[]
-} & S extends unknown
-  ? Record<string, any | any[] | Query.Prop<'any'>>
-  : {
-    [K in keyof S]?: S[K] extends Pick<Model.PropDesc<Model.Prop>, '$content'>
-      ? Model.GetPropType<S[K]> extends infer T
-        // @ts-ignore
-        ? Model.PropDescTypeMap[T] | Model.PropDescTypeMap[T][] | Query.Prop<T>
-        : never
+} & {
+  [K in keyof S]?: S[K] extends Pick<Model.PropDesc<Model.Prop>, '$content'>
+    ? Model.GetPropType<S[K]> extends infer T
+      // @ts-ignore
+      ? Model.PropDescTypeMap[T] | Model.PropDescTypeMap[T][] | Query.Prop<T>
       : never
-  }
+    : never
+}
 
 export namespace Query {
   export type Prop<T extends Model.PropDescType, VT = Model.PropDescTypeMap[T]> = {
