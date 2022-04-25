92cd3b8cbde02680c56dd9d6e192c60f270a731f
ijiefeng <yijie4188@gmail.com>
2022-04-25 14:56:02 +0800

    refactor(core): refactor query struct

--- packages/core/src/query.ts
+++ packages/core/src/query.ts
@@ -1,42 +1,34 @@
 import { Model } from './model'
 
-export interface Alias<T extends Model.PropDescType> {
-  as(alias: string): this
-  expr: Query.Prop<T>
-}
-
-export type Query<
-  T extends Model.PropDescType,
-  Props = Query.Prop<T>
-> = {
-  [K in keyof Props]: (v: Props[K]) => Alias<T>
-}
-
-// export const createQuery = <P extends Model.PropDesc<Model.Prop>['$content']>(
-//   name: string, prop: P
-// ) => {
-//   return new Proxy({} as Query<Exclude<P['type'], undefined>>, {
-//     get (t, p) {
-//       return (...args: any[]) => ({
-//       })
-//     }
-//   })
-// }
+export type Query<S extends Model.Schema | unknown> = {
+  $or?: Query<S>[]
+  $and?: Query<S>[]
+} & S extends unknown
+  ? Record<string, any | any[] | Query.Prop<'any'>>
+  : {
+    [K in keyof S]?: S[K] extends Pick<Model.PropDesc<Model.Prop>, '$content'>
+      ? Model.GetPropType<S[K]> extends infer T
+        // @ts-ignore
+        ? Model.PropDescTypeMap[T] | Model.PropDescTypeMap[T][] | Query.Prop<T>
+        : never
+      : never
+  }
 
 export namespace Query {
   export type Prop<T extends Model.PropDescType, VT = Model.PropDescTypeMap[T]> = {
+    $not?: Prop<T, VT>
   } & (
     T extends Model.Types.String | Model.Types.Number | Model.Types.Date ? {
-      eq: VT
-      ne: VT
-      gt: VT
-      lt: VT
-      gte: VT
-      lte: VT
+      $eq?: VT
+      $ne?: VT
+      $gt?: VT
+      $lt?: VT
+      $gte?: VT
+      $lte?: VT
     } : {}
   ) & (
     T extends Model.Types.String ? {
-      like: VT
+      $like?: VT
     } : {}
   )
 }
