fe6d72fcbe21a945494bed7e08b55cba40809420
ijiefeng <yijie4188@gmail.com>
2022-04-26 00:06:14 +0800

    refactor(core): refactor query and resolve it

--- packages/core/src/selector.ts
+++ packages/core/src/selector.ts
@@ -1,7 +1,37 @@
-import { Query } from './query'
 import { Model } from './model'
 
-type U2I<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never;
+type U2I<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never
+
+export type Query<S extends Model.Schema | unknown> = {
+  $or?: Query<S>[]
+  $and?: Query<S>[]
+} & {
+  [K in keyof S]?: S[K] extends Pick<Model.PropDesc<Model.Prop>, '$content'>
+    ? Model.GetPropType<S[K]> extends infer T
+      // @ts-ignore
+      ? Model.PropDescTypeMap[T] | Query.Prop<T>
+      : never
+    : never
+}
+
+export namespace Query {
+  export type Prop<T extends Model.PropDescType, VT = Model.PropDescTypeMap[T]> = {
+    $not?: Prop<T, VT>
+  } & (
+    T extends Model.Types.String | Model.Types.Number | Model.Types.Date ? {
+      $eq?: VT
+      $ne?: VT
+      $gt?: VT
+      $lt?: VT
+      $gte?: VT
+      $lte?: VT
+    } : {}
+  ) & (
+    T extends Model.Types.String ? {
+      $like?: VT
+    } : {}
+  )
+}
 
 export class Selector<
   Schemas extends readonly Model.Schema[], Props = U2I<Schemas[number]>,
@@ -26,5 +56,31 @@ export class Selector<
   offset(offset: number) {
     this.options.offset = offset
     return this
+  }
+}
+
+export namespace Selector {
+  export const resolve = <Queries extends readonly Query<Model.Schema>[]>(queries: Queries) => {
+    if (queries.length === 0) {
+      return {}
+    }
+    if (queries.length === 1) {
+      return queries[0]
+    }
+    return {
+      $or: Object.entries(queries).reduce((acc, [k, v]) => {
+        if (v) {
+          switch (typeof v) {
+            case 'object':
+              acc[k] = v
+              break
+            default:
+              acc[k] = { $eq: v }
+              break
+          }
+        }
+        return acc
+      }, {} as Query<Model.Schema>)
+    } as U2I<Queries>
   }
 }
