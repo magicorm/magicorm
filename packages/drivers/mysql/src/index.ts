ffcd64340e138b8026d793161de3346b23f48d3a
ijiefeng <yijie4188@gmail.com>
2022-05-09 17:43:31 +0800

    feat(driver-mysql): mysql driver

---
+++ packages/drivers/mysql/src/index.ts
@@ -0,0 +1,112 @@
+import { Connection, ConnectionConfig } from 'mysql'
+import {
+  AbsConnector,
+  AbsDriver,
+  Driver,
+  Engine,
+  Entity,
+  Model,
+  Selector,
+  UnconnectedDatabaseError
+} from '@magicorm/core'
+
+interface Options extends ConnectionConfig {
+  uri?: string
+}
+
+declare module '@magicorm/core' {
+  interface DriverOptionsMap {
+    'mysql': Options
+  }
+}
+
+const { createConnection } = require('@vlasky/mysql')
+
+class Connector extends AbsConnector<'mysql'> {
+  onConnect: Promise<void>
+  inner: Connection
+  constructor(options: Options) {
+    super(options)
+    if (options.uri)
+      this.inner = createConnection(options.uri)
+    else
+      this.inner = createConnection(options)
+
+    this.onConnect = new Promise<void>((resolve, reject) => {
+      this.inner.connect(err => {
+        if (err)
+          reject(err)
+        else
+          resolve()
+      })
+    })
+  }
+  close() {
+    return new Promise<void>((resolve, reject) => {
+      this.inner.end(err => {
+        if (err)
+          reject(err)
+        else
+          resolve()
+      })
+    })
+  }
+}
+
+class MysqlDriver extends AbsDriver<'mysql'> implements Driver<'mysql', Connector> {
+  constructor(options: Options) {
+    super('mysql', options)
+  }
+
+  exec(conn: Connector, sql: string, values?: any[]) {
+    return new Promise((resolve, reject) => {
+      conn.inner.query(sql, values, (err, results) => {
+        if (err) {
+          if (err.message === 'Cannot enqueue Query after invoking quit.') {
+            reject(new UnconnectedDatabaseError())
+            return
+          }
+          reject(err)
+        } else
+          resolve(results)
+      })
+    })
+  }
+
+  connect() {
+    return new Connector(this.options)
+  }
+
+  remove(m: Model, conn: Connector, opts?: Driver.OperateOptions) {
+  }
+
+  create(m: Model, conn: Connector, opts?: Driver.OperateOptions) {
+  }
+
+  delete<
+    Models extends Model[]
+  >(models: Models, query: Engine.Models2Query<Models>, conn: Connector, opts?: Driver.OperateOptions) {
+  }
+
+  insert(entities: Entity<Model>[], conn: Connector, opts?: Driver.OperateOptions) {
+    return []
+  }
+
+  search<
+    Schemas extends readonly Model.Schema[]
+  >(conn: Connector, opts?: Driver.OperateOptions, ...properties: Schemas) {
+    return new Selector(async () => [], ...properties)
+  }
+
+  update<
+    Models extends Model[]
+  >(models: Models, query: Engine.Models2Query<Models>, conn: Connector, opts?: Driver.OperateOptions) {
+  }
+
+  upsert<
+    Models extends Model[]
+  >(models: Models, query: Engine.Models2Query<Models>, conn: Connector, opts?: Driver.OperateOptions) {
+  }
+}
+
+export = MysqlDriver
---
+++ packages/drivers/mysql/tests/index.spec.ts
@@ -0,0 +1,31 @@
+import { expect, use } from 'chai'
+import cap from 'chai-as-promised'
+import { UnconnectedDatabaseError } from '@magicorm/core'
+import MysqlDriver from '@magicorm/driver-mysql'
+
+use(cap)
+
+after(() => {
+  process.exit(0)
+})
+
+describe('Mysql', function () {
+  it('should connect mysql', async () => {
+    const driver = new MysqlDriver({
+      user: 'root',
+      host: 'localhost',
+      port: 3306,
+      password: 'rYy(sql,1values,;4(err,'
+    })
+    const ctor = driver.connect()
+    await ctor.onConnect
+    expect(await driver.exec(ctor, 'select 1 as A'))
+      .to.be.deep.equal([{ A: 1 }])
+    expect(await driver.exec(ctor, 'select ? as A', [2]))
+      .to.be.deep.equal([{ A: 2 }])
+
+    await ctor.close()
+    await expect(driver.exec(ctor, 'select ? as A', [2]))
+      .to.be.eventually.rejectedWith(UnconnectedDatabaseError)
+  })
+})
