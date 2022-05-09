f19e4c94c065f1648c9335262f691fe60a82100f
ijiefeng <yijie4188@gmail.com>
2022-05-09 22:26:52 +0800

    feat(core): unknown database error

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -1,3 +1,9 @@
+export class UnknownDatabaseError extends Error {
+  constructor(name: string) {
+    super(`Unknown database '${name}'`)
+  }
+}
+
 export class UnconnectedDatabaseError extends Error {
   constructor() {
     super('The database is not connected, please check the connection status between the machine and the database')
