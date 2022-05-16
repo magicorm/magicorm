f06ab8939f3cc033f411e8e1ef87c51215029dc5
ijiefeng <yijie4188@gmail.com>
2022-05-16 20:50:36 +0800

    feat(driver-mysql): unit test static method for MysqlDriver

--- packages/drivers/mysql/tests/index.spec.ts
+++ packages/drivers/mysql/tests/index.spec.ts
@@ -1,6 +1,6 @@
 import { expect, use } from 'chai'
 import cap from 'chai-as-promised'
-import { UnconnectedDatabaseError } from '@magicorm/core'
+import { createModel, dp, UnconnectedDatabaseError } from '@magicorm/core'
 import MysqlDriver from '@magicorm/driver-mysql'
 
 use(cap)
@@ -23,6 +23,11 @@ const connectDriver = async () => {
 }
 
 describe('Mysql', function () {
+  const User = createModel('user', {
+    id: dp('int').primary.autoinc,
+    name: dp('string').notnull.unique,
+    age: dp('int').notnull
+  })
   it('should connect.', async () => {
     const [driver, ctor] = await connectDriver()
     expect(await driver.exec(ctor, 'select 1 as A'))
@@ -33,5 +38,17 @@ describe('Mysql', function () {
     await ctor.close()
     await expect(driver.exec(ctor, 'select ? as A', [2]))
       .to.be.eventually.rejectedWith(UnconnectedDatabaseError)
+  })
+  it('should create table and drop it.', async () => {
+    const [driver, ctor] = await connectDriver()
+    driver.create(User, ctor)
+  })
+  describe('Static', function () {
+    it('should generate create table sql.', async () => {
+      expect(MysqlDriver.resolveSchema(User.name, User.schema))
+        .to.be.eq(
+          'create table `user` (`id` int auto_increment, constraint user_pk primary key (id), `name` varchar not null, `age` int not null);'
+        )
+    })
   })
 })
